f4302b7772d0af38f90263622e53a8019b5701fa
owenjonesuob <olj23@bath.ac.uk>
2018-01-21 10:37:54 +0000

    Test abbrs parameter in get_range_crude

--- inst/tests/testthat/test-adding_records.R
+++ inst/tests/testthat/test-adding_records.R
@@ -20,7 +20,7 @@ test_that("Adding records for the first time", {
 
     
     
-    raw_range <- get_range_crude("2015-01-01 06:00:00", "2015-01-01 12:00:00")
+    raw_range <- get_range_crude("2015-01-01 06:00:00", "2015-01-01 12:00:00", abbrs = "a")
     
     expect_equal(nrow(raw_range), 360)
     expect_equal(as.character(min(raw_range$dateuploaded)), "2015-01-01 06:00:01")
